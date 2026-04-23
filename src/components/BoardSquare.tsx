c11b9f4362cfc4a9ccb150c5f98e15c771bbfe98
Michelle Wrighton <MWrighton@users.noreply.github.com>
2026-04-23 08:49:15 -0400

    Free Space CSS implemented.

--- src/components/BoardSquare.tsx
+++ src/components/BoardSquare.tsx
@@ -2,7 +2,7 @@ import './Board.css'
 
 export default function BoardSquare(props: {bingoText: string, isFree?: boolean}) {
   return (
-    <div className='bingo-box'>
+    <div className={`bingo-box ${props.isFree && 'free-space'}`}>
         {props.isFree ? "FREE SPACE" : props.bingoText}
     </div>
   );
